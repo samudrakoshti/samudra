6f6ffd7e69487d06bdcedfc75c150702ee42340a
smaudra koshti <samudrakoahti@gmail.com>
2025-07-31 18:45:26 +0530

    Create python3.py
    #day4 of my python journey,
    #About Dictionary in python.

---
+++ python3.py
@@ -0,0 +1,21 @@
+items=[{
+    "name":"samudra",
+    "age":19,
+    "class":"BE 1st year"
+},
+      {
+           "name": "sagar",
+           "age": 17,
+           "class": "11th"
+
+       },
+    {
+       "name":"ravi",
+        "friends":["mahesh","ram","shri"],
+        ("age","class"):[19,"1st year"]
+    }
+]
+
+print(f"sum of age of two person is: {items[0]["age"]+items[1]["age"]}:")
+print(items[2]["friends"])
+print(items[0].get("friends","not found"))
