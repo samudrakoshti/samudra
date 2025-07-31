2ed9f3847534c4e3d2e154e32f41dab8f81e1855
smaudra koshti <samudrakoahti@gmail.com>
2025-07-31 18:47:20 +0530

    Create python4.py
    #day4 of my python journey!
    #About if,elif,else

---
+++ python4.py
@@ -0,0 +1,14 @@
+N=int(input("ENTER VALUE: "))
+if N%2==0:
+    print(f"{N} is even")
+else:
+    print(f"{N} is odd")
+    print("------TRAFFIC SIGNAL-------")
+C=input("ENTER SIGNAL COLOR: ")
+if C=="red":
+    print("STOP: ")
+elif C=="yelow":
+    print("READY: ")
+else:
+    print("GO: ")
+
